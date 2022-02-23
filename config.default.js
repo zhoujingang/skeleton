74e5ec699f8511c48e08dab56253866eb9272908
zhoujingang1 <zhoujingang1@jd.com>
2022-02-23 14:18:23 +0800

    fix:骨架屏脚手架

---
+++ config.default.js
@@ -0,0 +1,25 @@
+module.exports = {
+    url: 'http://www.baidu.com', // 页面地址
+
+    devices: {               // 支持设备类型列表
+        iphone: [375, 667, 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'],
+        ipad: [768, 1024, 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'],
+        ipadPro: [1024, 1366, 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'],
+        pc: [1200, 1000, 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1']
+    },
+    device: 'iphone',        // 默认设备类型
+    style: `animation: opacity 1s linear infinite;`,
+    background: '#eee',
+    output: '',
+    // headers: { 
+    //     cookies: [{ // 设置 cookie
+    //         'name': 'pt_pin',
+    //         domain: '.jd.com',
+    //         'value': '%E6%88%91%E5%90%83%E5%B0%8F%E8%98%91%E8%8F%87635'
+    //     }, {
+    //         'name': 'pt_key',
+    //         domain: '.jd.com',
+    //         'value': 'AAJiEzi8AECSC83BcZNPs3bji3v7LxJVFpr-GMBV9dXDfSCtDK64orA4FUQr9QopjF23IDsERxPlQcTBCEanx8p3JNhcJ2BY'
+    //     }],
+    // }
+}
---
+++ index.js
@@ -0,0 +1,77 @@
+
+const fs = require('fs-extra');
+const path = require('path');
+const Puppeteer = require('./lib/pupeteer');
+const drawSkeleton = require('./lib/drawSkeleton');
+const configDefault = require('./config.default');
+const cheerio = require('cheerio');
+const defaultHtml = require('./html.default');
+const ora = require('ora');
+class Skeleton {
+    constructor(options) {
+        // TODO 参数校验 joi
+        this.root = "#root";
+        // this.openBrowser()
+
+    }
+
+    /**
+     * 初始化配置
+     */
+    async init() {
+        try {
+            const str = fs.readFileSync(path.resolve(__dirname, './config.default.js'), 'utf8');
+            await fs.writeFileSync(path.resolve(process.cwd(), './config.js'), str)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    /**
+     * 绘制骨架屏
+     */
+    async openBrowser() {
+        // TODO 参数校验
+        let config = require(path.resolve(process.cwd(), './config.js'))
+        config = Object.assign(configDefault, config);
+        const { url } = config;
+        if (!url) {
+            throw new Error('url is required!')
+        }
+        const browser = new Puppeteer(config);
+        await browser.launch();
+        const page = await browser.newPage(config.url, config.headers);
+        const html = await page.evaluate(drawSkeleton, config);
+        const $ = cheerio.load(defaultHtml);
+        $(this.root).html(html);
+        await fs.writeFileSync(path.resolve(process.cwd(), './index.html'), $.root().html())
+    }
+
+}
+
+const start = (argv) => {
+    const spinner = ora('请稍后...').start();
+    const command = process.argv.slice(2)[0]
+    const skeleton = new Skeleton(argv);
+    switch (command) {
+        case 'init':
+            skeleton.init();
+            spinner.info('配置加载中...')
+            spinner.color = 'green';
+            setTimeout(() => {
+                spinner.succeed('配置加载成功,在当前目录下 config.js 中可自定义定制骨架屏！')
+            }, 500)
+            break;
+        case 'start':
+            spinner.info('骨架屏生成中...')
+            skeleton.openBrowser();
+            setTimeout(() => {
+                spinner.succeed('骨架屏绘制成功！已保存至当前目录 index.html!')
+            }, 500)
+            break;
+        default:
+            spinner.warn('不存在该执行命令！') ;
+    }
+}
+
+module.exports = start;
